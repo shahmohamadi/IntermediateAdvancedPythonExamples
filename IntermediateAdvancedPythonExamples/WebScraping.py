5d3eaa8e502329ff852a367da491ca7dbc342690
samane <samane.shahmohamadi@gmail.com>
2019-08-01 21:15:59 +0430

    add WebScrapping.py

--- IntermediateAdvancedPythonExamples/WebScraping.py
+++ IntermediateAdvancedPythonExamples/WebScraping.py
@@ -1,6 +1,7 @@
 # working with HTML parser
 import requests
 from bs4 import BeautifulSoup
+import re
 
 r = requests.get('http://srbiau.ac.ir/fa/news/category/100/%DA%A9%D8%A7%D8%B1%DA%AF%D8%A7%D9%87-%D9%87%D8%A7')
 # print(r.text)
@@ -13,7 +14,9 @@ val = soup.find_all('h3')
 # print(val)
 val1 = val[1]
 # print(val1)
-print(val1.attrs)
-val = soup.find_all('h3', attrs={'class': 'title'})
-print(val)
+# print(val1.attrs)
+result = soup.find_all('h3', attrs={'class': 'title'})
+print(result)
+
+
 
